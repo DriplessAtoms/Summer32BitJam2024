0de064e1b4d609bbfc01fcdf10025dbaaff10e31
DriplessAtoms <78892280+DriplessAtoms@users.noreply.github.com>
2024-07-27 05:08:51 -0700

    Pause Menu Audio Settings Added

--- Summer32BitJam2024/Assets/Scripts/Menu/PauseMenu.cs
+++ Summer32BitJam2024/Assets/Scripts/Menu/PauseMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -9,33 +10,51 @@ public class PauseMenu : MonoBehaviour
     // Making this a global var just in case you need this.
     public static bool isPaused;
 
+    public AudioMixer mainMixer;
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape)) {
-            if (isPaused) {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
                 ResumeGame();
             }
-            else {
+            else
+            {
                 PauseGame();
             }
         }
     }
 
-    public void PauseGame() {
+    public void PauseGame()
+    {
         pauseMenu.SetActive(true);
         Time.timeScale = 0f;
         isPaused = true;
     }
 
-    public void ResumeGame() {
+    public void ResumeGame()
+    {
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;
     }
 
-    public void Menu() {
+    public void Menu()
+    {
         Time.timeScale = 1f;
         SceneManager.LoadSceneAsync(0);
+    }
+
+    public void SetVolumeMusic(float sliderValue)
+    {
+        mainMixer.SetFloat("Music", sliderValue);
+    }
+
+    public void SetVolumeSFX(float sliderValue)
+    {
+        mainMixer.SetFloat("SFX", sliderValue);
     }
 }
